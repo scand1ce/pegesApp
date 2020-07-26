faf321f401843df28880eaf36e2963efd1735072
Serhii_Tomchuk <dice1@ukr.net>
2020-07-26 22:00:40 +0300

    'djhe'

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -56,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],  # new,
-        'APP_DIRS': True,
+        'APP_DIRS': [django_heroku.settings(locals())],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
